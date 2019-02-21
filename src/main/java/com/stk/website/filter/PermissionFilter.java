f38efbf554a78d8976a997c6f47b79158060fe9c
yayayahhh <huangrongya@foxmail.com>
2019-02-21 21:09:32 +0800

    权限过滤

--- src/main/java/com/stk/website/filter/PermissionFilter.java
+++ src/main/java/com/stk/website/filter/PermissionFilter.java
@@ -8,6 +8,7 @@ import com.stk.website.exception.ServiceException;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 public class PermissionFilter implements Filter {
     @Override
@@ -19,13 +20,17 @@ public class PermissionFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         String url = request.getRequestURI();
+        if (url.contains("login")){
+            String name = request.getParameter("name");
+            String token = UUID.randomUUID().toString().replace("-","");
+            Global.tokenMap.put(name, token);
+        }
         if (url.contains("manage")){
             String token = (String) request.getSession().getAttribute("token");
             String userName = (String) request.getSession().getAttribute("name");
             if (token == null || !token.equals(Global.tokenMap.get(userName))){
                 throw new ServiceException(ErrorConstant.NO_PERMISSION_CODE, ErrorConstant.NO_PERMISSION_MSG);
             }
-
         }
         filterChain.doFilter(request, servletResponse);
     }
