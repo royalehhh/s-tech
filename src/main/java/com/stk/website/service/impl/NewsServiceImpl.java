2b36835a798448a66484be59534a846471ff67de
yayayahhh <huangrongya@foxmail.com>
2019-03-07 21:08:43 +0800

    更新新闻简介

--- src/main/java/com/stk/website/service/impl/NewsServiceImpl.java
+++ src/main/java/com/stk/website/service/impl/NewsServiceImpl.java
@@ -30,6 +30,11 @@ public class NewsServiceImpl implements INewsService {
         example.setOrderByClause("id");
         List<News> list = newsMapper.selectByExampleWithBLOBs(example);
         long total = newsMapper.countByExample(example);
+        if (!list.isEmpty()){
+            for (News news : list) {
+                news.setIntroduction(news.getIntroduction().replace(System.lineSeparator(),"<br/>").replace("\t", " "));
+            }
+        }
         response.setPageList(list);
         response.setTotalCount(total);
         response.setTotalPage(Global.getTotalPage(total, request.getRow()));
@@ -45,6 +50,8 @@ public class NewsServiceImpl implements INewsService {
             response.setMsg(ErrorConstant.DATABASE_NO_DATA_MSG);
             return response;
         }
+        news.setContent(news.getContent().replace(System.lineSeparator(),"<br/>").replace("\t", " "));
+        news.setIntroduction(news.getIntroduction().replace(System.lineSeparator(),"<br/>").replace("\t", " "));
         response.setNews(news);
         return response;
     }
