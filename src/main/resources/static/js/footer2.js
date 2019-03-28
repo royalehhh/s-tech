192168075411e4097beefa5dc48f12e9a49d077c
huangrongya <huangrongya@foxmail.com>
2019-03-28 14:23:25 +0800

    修改一些静态内容

--- src/main/resources/static/js/footer2.js
+++ src/main/resources/static/js/footer2.js
@@ -1,3 +1,3 @@
-(function($){
-	$(".foot2").html('<div class="body_1182"><span class="FL">Copyright © 2017圣泰克版权所有 浙ICP备17038718号</span><font class="yanse" style="color:red; padding:0 5px;">技术支持：<a target="_blank" href="http://www.cheerhen.com/">千恒科技</a></font><span class="FR">设计制作:千恒科技</span></div>');
+(function ($) {
+    $(".foot2").html('<div class="body_1182"><span class="FL">Copyright © 2019申瓯版权所有 浙ICP备17038718号</span><font class="yanse" style="color:red; padding:0 5px;"></span></div>');
 })(jQuery)
